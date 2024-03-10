3f1bd5dc5f47f562e4b665c1f99369d280b727ec
Cristian Alzate <calzateu@eafit.edu.co>
2024-03-09 21:36:55 -0500

    refactor: uncommented synthetic data code.

--- Project_2/Code/main.py
+++ Project_2/Code/main.py
@@ -7,21 +7,24 @@ import synthetic_data
 
 
 if __name__ == '__main__':
-    # Generate synthetic data
-    # Specify min and max values for each variable
-    # min_vals = [0, 0, 0, 0]
-    # max_vals = [1, 24, 49, 49]
 
-    # # Generate 10000 samples
-    # num_samples = 10000
+    generate_synthetic_data = False
 
-    # synthetic_data_prbs = synthetic_data.generate_synthetic_data_prbs(
-    #     num_samples, min_vals, max_vals, graph=False, save_results=False
-    #     )
+    if generate_synthetic_data:
+        # Specify min and max values for each variable.
+        min_vals = [0, 0, 0, 0]
+        max_vals = [1, 24, 49, 49]
 
-    # Get results
-    # synthetic_data_uniform = synthetic_data.generate_synthetic_data(min_vals, max_vals, num_samples,
-    #                                          save_results=False)
+        # Generate 10000 samples
+        num_samples = 10000
+
+        synthetic_data_prbs = synthetic_data.generate_synthetic_data_prbs(
+            num_samples, min_vals, max_vals, graph=False, save_results=False
+            )
+
+        # Get results
+        synthetic_data_uniform = synthetic_data.generate_synthetic_data(min_vals, max_vals, num_samples,
+                                                 save_results=False)
 
     # Read synthetic data
     data = io.read_synthetic_data("Project_2/outputs/output_centroid.csv")
@@ -33,8 +36,8 @@ if __name__ == '__main__':
     subsample = dp.normalize(subsample)
 
     distances_euclidean = dp.compute_distances(subsample, norms.euclidean_norm)
-    #distances_manhattan = dp.compute_distances(subsample, norms.manhattan_norm)
-    #distances_chebyshev = dp.compute_distances(subsample, norms.p_norm, 2)
+    # distances_manhattan = dp.compute_distances(subsample, norms.manhattan_norm)
+    # distances_chebyshev = dp.compute_distances(subsample, norms.p_norm, 2)
 
     print(distances_euclidean)
     print(len(distances_euclidean))
