f37b7dd302429a0e92a4969b2890e115475838ee
Cristian Alzate <calzateu@eafit.edu.co>
2024-03-13 15:45:06 -0500

    feat: added documentation.

--- project_2/src/clustering.py
+++ project_2/src/clustering.py
@@ -6,11 +6,37 @@ from typing import Callable
 
 
 def __calc_mountain_term(vector1, vector2, norm, constant):
+    """
+    Calculate the mountain term between two vectors using the provided norm function and
+    the provided constant (it can be sigma or beta).
+
+    Parameters:
+        vector1 (numpy array): The first input vector.
+        vector2 (numpy array): The second input vector.
+        norm (function): The norm function to calculate the distance between the vectors.
+        constant (float): The constant used in the calculation.
+
+    Returns:
+        float: The calculated mountain term.
+    """
     return np.exp(-((norm(vector1, vector2))**2)/(2*constant**2))
 
 
 def mountain_clustering(data: pd.DataFrame, norm: Callable, sigma: float, beta: float,
                         graphics: bool = False) -> list[int]:
+    """
+    Perform mountain clustering on the given data.
+
+    Args:
+        data (pd.DataFrame): The input data for clustering.
+        norm (Callable): The normalization function.
+        sigma (float): The sigma parameter.
+        beta (float): The beta parameter.
+        graphics (bool, optional): Whether to display graphics. Defaults to False.
+
+    Returns:
+        list[int]: The indices of the cluster centers.
+    """
     # First step: create grid (mesh)
     array = np.linspace(0, 1, 3)
     grid = mesh.create_mesh(array, 4)
