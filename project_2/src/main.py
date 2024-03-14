b590834a4e40b7f20e62b61fd37b2e613393c71b
Cristian Alzate <calzateu@eafit.edu.co>
2024-03-13 22:38:54 -0500

    fix: fixed issue when resampling is applied.

--- project_2/src/main.py
+++ project_2/src/main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     run_clustering = True
     dataset_name = "Iris dataset"
     drop_axes = ["Id", "Species"]
-    sub_sample_size = 150
+    sub_sample_size = None
     axes = ["SepalLengthCm", "SepalWidthCm"]
 
     # Choose if you want to run distance calculations and plot graphs.
@@ -17,5 +17,5 @@ if __name__ == '__main__':
 
     run_examples.run_unsupervised_pipeline(generate_synthetic_data=generate_synthetic_data,
                                            run_clustering=run_clustering,  dataset_name=dataset_name,
-                                           subsample_size=None, drop_axes=drop_axes, axes=axes,
+                                           subsample_size=sub_sample_size, drop_axes=drop_axes, axes=axes,
                                            run_distances=run_distances)
