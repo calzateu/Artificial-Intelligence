fix: added cost derivative.

project_3/homeworks/neural_network.py
@@ -15,6 +15,9 @@ class Layer:
         # Binary cross entropy loss
         return -(targets * np.log(self.forwarded_inputs) + (1 - targets) * np.log(1 - self.forwarded_inputs))
 
+    def derivate_output_error(self, targets):
+        return - targets / self.forwarded_inputs + (1 - targets) / (1 - self.forwarded_inputs)
+
     def update_neurons(self, input_data, output_error, learning_rate):
         # for neuron, error in zip(self.neurons, output_error):
         #     neuron.update_weights(input_data, error, learning_rate)
@@ -49,7 +52,9 @@ class NeuralNetwork:
         return output
 
     def backward(self, input_data, targets):
-        output_error = self.layers[-1].calculate_output_error(targets)
+        # output_error = self.layers[-1].calculate_output_error(targets)
+        # output_error = - targets / self.forward(input_data) + (1 - targets) / (1 - self.forward(input_data))
+        derivative_output_error = self.layers[-1].derivate_output_error(targets)
         #for i in reversed(range(len(self.layers) - 1)):
         for i in [len(self.layers) - 1]:
             if i == 0:
@@ -57,7 +62,7 @@ class NeuralNetwork:
             else:
                 input_data_backward = self.layers[i - 1].forwarded_inputs
             layer = self.layers[i]
-            layer.update_neurons(input_data_backward, output_error, self.learning_rate)
+            layer.update_neurons(input_data_backward, derivative_output_error, self.learning_rate)
 
 
 # neural_network = NeuralNetwork(n_inputs=1, hidden_layers=[10, 10], n_outputs=1, learning_rate=0.01)
@@ -78,4 +83,3 @@ for i in range(1000):
 
 print(neural_network.layers)
 print(neural_network.forward(input))
-

project_3/homeworks/neuron_module.py
@@ -24,4 +24,4 @@ class Neuron:
         v = self.__linear_combination(inputs)
         delta = error * self.__activation_derivative(v)
         weight_deltas = learning_rate * np.dot(delta, inputs)
-        self.weights += weight_deltas
+        self.weights -= weight_deltas

project_3/homeworks/perceptron_entropy.py
@@ -28,7 +28,8 @@ def gradient_descent(X, y, w, learning_rate, epochs):
     for _ in range(epochs):
         v = local_camp(X, w)
         y_pred = activation_function(v)
-        grad = np.dot(y_pred - y, X)  # Ver de dónde se obtiene en el pdf adjunto
+        # grad = np.dot(y_pred - y, X)  # Ver de dónde se obtiene en el pdf adjunto
+        grad = np.dot((-y / y_pred + (1 - y) / (1 - y_pred)) * derivative_activation_function(v), X)
         print(grad)
         w = w - learning_rate * grad
 