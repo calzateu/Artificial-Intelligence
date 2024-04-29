789f7eff07f9f3d9e9e651b7dd178fb4568a68f0
Cristian Alzate <calzateu@eafit.edu.co>
2024-04-28 22:16:12 -0500

    refactor: code improved.

--- project_3/src/main.py
+++ project_3/src/main.py
@@ -3,53 +3,58 @@ import data_processing as dp
 import neural_network as nn
 import pandas as pd
 
-n_outputs = 2
-neural_network = nn.NeuralNetwork(n_inputs=3, hidden_layers=[2], n_outputs=n_outputs, learning_rate=1)
 
-# inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-# targets = np.array([[0], [1], [1], [1]])
-# targets = np.array([[0, 1], [1, 0], [0, 1], [1, 0]])
-# inputs = np.array([[0, 0]])
-# targets = np.array([[0]])
+def main():
+    n_outputs = 2
+    neural_network = nn.NeuralNetwork(n_inputs=3, hidden_layers=[2], n_outputs=n_outputs, learning_rate=1)
 
-data = pd.read_csv("../data/data.csv")
+    # Data for testing the neural network with the XOR function
+    # inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    # targets = np.array([[0], [1], [1], [1]])
+    # targets = np.array([[0, 1], [1, 0], [0, 1], [1, 0]])
 
-print("Original data: ")
-print(data.head())
+    data = pd.read_csv("../data/data.csv")
 
-data_copy = data.copy()
-data_copy = dp.preprocess_data(data_copy)
+    print("Original data: ")
+    print(data.head())
 
-print("Preprocessed data: ")
-print(data_copy.head())
+    data_copy = data.copy()
+    data_copy = dp.preprocess_data(data_copy)
 
-print()
+    print("Preprocessed data: ")
+    print(data_copy.head())
 
-inputs = data_copy[['X0', 'X1', 'X2']].to_numpy()
-targets = data_copy[['X3', 'X4']].to_numpy()
+    print()
 
-print("Score before training: ", neural_network.score(inputs, targets))
+    inputs = data_copy[['X0', 'X1', 'X2']].to_numpy()
+    targets = data_copy[['X3', 'X4']].to_numpy()
 
-epochs = 10
-save_local_gradients = True
-errors = neural_network.train(inputs, targets, epochs=epochs, save_local_gradients=save_local_gradients)
+    print("Score before training: ", neural_network.score(inputs, targets))
 
-print("Score after training: ", neural_network.score(inputs, targets))
+    epochs = 10
+    save_local_gradients = True
+    errors = neural_network.train(inputs, targets, epochs=epochs, save_local_gradients=save_local_gradients)
 
-print()
+    print("Score after training: ", neural_network.score(inputs, targets))
 
-outputs = neural_network.predict_dataset(inputs)
+    print()
 
-observations = [0, 1, 2, 900]
-for observation in observations:
-    print(f"Target at observation {observation}: {targets[observation]} and output: {outputs[observation]}")
+    outputs = neural_network.predict_dataset(inputs)
 
-print()
-print("Plotting results...")
-print()
+    observations = [0, 1, 2, 900]
+    for observation in observations:
+        print(f"Target at observation {observation}: {targets[observation]} and output: {outputs[observation]}")
 
-gr.plot_energy(epochs, errors, number_of_data_points=len(inputs))
-gr.plot_errors(epochs, errors, number_of_data_points=len(inputs))
+    print()
+    print("Plotting results...")
+    print()
 
-if save_local_gradients:
-    gr.plot_local_gradients(neural_network, epochs, number_of_data_points=len(inputs))
+    gr.plot_energy(epochs, errors, number_of_data_points=len(inputs))
+    gr.plot_errors(epochs, errors, number_of_data_points=len(inputs))
+
+    if save_local_gradients:
+        gr.plot_local_gradients(neural_network, epochs, number_of_data_points=len(inputs))
+
+
+if __name__ == "__main__":
+    main()
